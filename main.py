684348c7e54665a1d072d5d5846f3716600fac14
John Taylor <johntaylormax@outlook.com>
2023-12-23 20:47:11 -0500

    fixed some text on page

--- main.py
+++ main.py
@@ -42,28 +42,29 @@ def query_similar_documents(client, embed, index, query, top_k=5):
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 
-openai_client, embed_model, pinecone_index = initialize_clients()
+# openai_client, embed_model, pinecone_index = initialize_clients()
 # Initialize clients
 
 # #streamlit header
-st.title("Document Search")
+st.title("Legal Case Search")
 
 #create two columns
 col1, col2 = st.columns([2,1])
 with col1:
     # query = st.text_input("Enter your query here:")
     #make teh query box into a wrapping text box with multi line capability
-    query = st.text_area("Enter the facts of your legal situation here:", height=200)
+    query = st.text_area("Enter the facts of your legal matter here:", height=200)
     #check the spelling of the query
 with col2:
     #insert three radio butons labeled 5 10 and 15
-    top_k = st.radio("How many results do you want?", [5, 10, 15])
+    top_k = st.radio("Maximum results", [5, 10, 15])
     #convert top_k to an integer
     top_k = int(top_k)
 
 search_button = st.button("Search")
 # #streamlit button
 if search_button and query:
+    openai_client, embed_model, pinecone_index = initialize_clients()
     results = query_similar_documents(openai_client, embed_model, pinecone_index, query, top_k)
     if results:
         search_complete = st.write("Search complete!")
@@ -90,4 +91,4 @@ if search_button and query:
                 st.markdown("---")
         st.write(results['matches'])
 elif search_button and not query:
-    error_notice = st.write("Please enter the facts of your legal situation and click the search button.")
+    error_notice = st.write("Please enter the facts of your legal matter and click search.")
